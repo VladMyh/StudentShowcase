9ecd389ea0939dcb353c5d8a07fa33b3f6f93351
vlad <vladmyh@gmail.com>
2016-12-12 19:44:50 +0200

    Fixed issue with running artifacts built by maven

---
+++ frontend/src/main/java/com/studentshowcase/frontend/FrontendApplication.java
@@ -0,0 +1,12 @@
+package com.studentshowcase.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+}
