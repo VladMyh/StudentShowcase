9e5b8aad45d08daa005a577c8b5f7c009382ef4f
vlad <vladmyh@gmail.com>
2016-12-15 19:42:09 +0200

    frontend spring jar now serves amgular app

--- frontend/src/main/java/com/studentshowcase/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/studentshowcase/frontend/FrontendApplication.java
@@ -1,66 +1,32 @@
 package com.studentshowcase.frontend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
-import java.util.UUID;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @Controller
 public class FrontendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
 	}
 
-	@RequestMapping(value = "/{path:[^\\.]*}")
-	public String redirect() {
-		return "forward:/";
-	}
-
 	@RequestMapping("/user")
-	@ResponseBody
-	public Principal user(Principal user) {
-		return user;
+	public Map<String, String> user(Principal user) {
+		return Collections.singletonMap("name", user.getName());
 	}
 
-	@RequestMapping("/resource")
-	@ResponseBody
-	public Map<String, Object> home() {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
+	@Bean
+	public JdkMongoSessionConverter jdkMongoSessionConverter() {
+		return new JdkMongoSessionConverter();
 	}
-
-	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic().disable();
-			http
-
-					.authorizeRequests()
-					.antMatchers("/index.html", "/", "/login", "/main").permitAll()
-					.anyRequest().authenticated()
-					.and()
-					.csrf()
-					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-		}
-	}
-
-
 }
