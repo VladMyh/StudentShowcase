64be9305323def6768c0dd6104d8b7b61f51ab54
vlad <vladmyh@gmail.com>
2016-12-18 14:19:13 +0200

    Fromtend and rest service now work through api gateway, started implementing student registration in frontend

--- rest-service/src/main/java/com/studentshowcase/restcontroller/Application.java
+++ rest-service/src/main/java/com/studentshowcase/restcontroller/Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @ComponentScan(basePackages = {"com.studentshowcase"})
 @SpringBootApplication
@@ -16,13 +13,5 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().disable().csrf()
-						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/**").hasRole("WRITER")
-								.antMatchers(HttpMethod.GET, "/**").hasRole("READER")
-								.anyRequest().authenticated();
 	}
 }
