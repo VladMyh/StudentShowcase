ac16670d72b05644abae05d3cec0b169860545db
vlad <vladmyh@gmail.com>
2017-01-06 19:19:31 +0200

    Started implementing clientside authentication

--- frontend/src/main/java/com/studentshowcase/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/studentshowcase/frontend/FrontendApplication.java
@@ -3,14 +3,7 @@ package com.studentshowcase.frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
-import java.util.Collections;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -18,15 +11,5 @@ import java.util.Map;
 public class FrontendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
-	}
-
-	@RequestMapping("/user")
-	public Map<String, String> user(Principal user) {
-		return Collections.singletonMap("name", user.getName());
-	}
-
-	@Bean
-	public JdkMongoSessionConverter jdkMongoSessionConverter() {
-		return new JdkMongoSessionConverter();
 	}
 }
--- frontend/src/main/resources/static/services/userauthservice.js
+++ frontend/src/main/resources/static/services/userauthservice.js
@@ -7,25 +7,33 @@ function AuthService($http, Session) {
     var authService = {};
 
     authService.login = function (credentials) {
-        return $http
-            .post('/api/auth', credentials)
-            .then(function (res) {
-                //Session.create(res.data.id, res.data.user.id, res.data.user.role);
-                //return res.data.user;
 
+        var headers = credentials ? {authorization : "Basic "
+        + btoa(credentials.email + ":" + credentials.password)
+        } : {};
+
+        return $http
+            .get('/api/user', {headers : headers})
+            .then(function (res) {
+                Session.create(res.data.id, res.data.user.id, res.data.user.role);
+                return res.data.user;
+                console.log("Creating session: " + res.data.key);
             });
     };
 
     authService.isAuthenticated = function () {
         //return !!Session.userId;
+        console.log("isAuthenticated");
     };
 
     authService.isAuthorized = function (authorizedRoles) {
-        if (!angular.isArray(authorizedRoles)) {
-            authorizedRoles = [authorizedRoles];
-        }
-        return (authService.isAuthenticated() &&
-        authorizedRoles.indexOf(Session.userRole) !== -1);
+        // if (!angular.isArray(authorizedRoles)) {
+        //     authorizedRoles = [authorizedRoles];
+        // }
+        // return (authService.isAuthenticated() &&
+        // authorizedRoles.indexOf(Session.userRole) !== -1);
+
+        console.log("isAuthorized");
     };
 
     return authService;
